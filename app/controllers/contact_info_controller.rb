2a825cc2377557569d76235e0fa279b2c9a46f17
kvkkalyan <kalyan99.koppolu@gmail.com>
2015-10-16 15:00:23 +0000

    configured environment to work with heroku

---
+++ app/controllers/contact_info_controller.rb
@@ -0,0 +1,4 @@
+class ContactInfoController < ApplicationController
+  def index
+  end
+end
---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,11 @@
+require 'open_weather'
+class WeatherController < ApplicationController
+  def index
+  end
+  
+  def info
+    options = { units: "metric", APPID: '3e08f352ed070370999ae1ee9ddd1dff' }
+  	 @city_name = params[:city_name]
+  	 @weather_details = OpenWeather::Current.city(@city_name,options)
+  end
+end
